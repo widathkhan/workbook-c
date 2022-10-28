bfa715d3c8ddc1f7251ef936055704bef9f5c5a0
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 20:47:52 +0530

    Create Multiplevalues4.c

---
+++ Multiplevalues4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main() 
+{ 
+ int a, b, c; 
+ printf("Enter two numbers to add\n"); 
+ scanf("%d%d",&a,&b); 
+ c = a + b; 
+ printf("Sum of entered numbers = %d\n",c); 
+ 
+} 
