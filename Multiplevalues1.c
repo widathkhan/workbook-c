7d51f72d5599c257d787f4b11d5901f790231542
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 20:46:24 +0530

    Create Multiplevalues1.c

---
+++ Multiplevalues1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{ 
+ int a; 
+ float b; 
+ printf("Enter integer and then a float: ");
+ scanf("%d%f", &a, &b); 
+ printf("You entered %d and %f", a, b); 
+ return 0; 
+} 
+ 
